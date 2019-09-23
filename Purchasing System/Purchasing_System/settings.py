b66119b6d6f701134af19905eea6604418fef6e1
jacktiew96 <jacktiew_0507@hotmail.com>
2019-09-23 10:57:10 +0800

    fix email version v2

--- Purchasing System/Purchasing_System/settings.py
+++ Purchasing System/Purchasing_System/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '1142702277@student.mmu.edu.my'
-EMAIL_HOST_PASSWORD = 'kKl@!9471215'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
